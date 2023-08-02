1bac90b3ebb8172f7ff7ab9c042b40f79ac3e297
ArnoldSoos <arnoldsoos7@gmail.com>
2023-08-02 23:37:49 +0300

    test commit

--- database/seeders/PostsTagsSeeder.php
+++ database/seeders/PostsTagsSeeder.php
@@ -6,27 +6,27 @@ use Illuminate\Database\Seeder;
 
 class PostsTagsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $posts = \App\Models\Post::latest()->take(10)->get();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $posts = \App\Models\Post::latest()->take(10)->get();
 
-        $tags_ids = \App\Models\Tag::pluck('id')->toArray();
+    $tags_ids = \App\Models\Tag::pluck('id')->toArray();
 
-        foreach ($posts as $post) {
+    foreach ($posts as $post) {
 
-            // get a random number between 1 and 4
-            $rand_nr = rand(1, 4);
+      // get a random number between 1 and 4
+      $rand_nr = rand(1, 4);
 
-            for($i = 0; $i < $rand_nr; $i++) {
-                // get a random tag id, and attach to actual post
-                $post->tags()->attach($tags_ids[array_rand($tags_ids)]);
-            }
-            
-        }
+      for($i = 0; $i < $rand_nr; $i++) {
+          // get a random tag id, and attach to actual post
+          $post->tags()->attach($tags_ids[array_rand($tags_ids)]);
+      }
+        
     }
+  }
 }
